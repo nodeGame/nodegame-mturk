f78ec1fb9822c59890dd3e5385d1bcd7b080b846
Stefano Balietti <futur.dorko@gmail.com>
2016-09-09 17:05:38 -0400

    first stage

--- ng-amt.js
+++ ng-amt.js
@@ -5,15 +5,9 @@
 // General.
 var fs = require('fs-extra');
 var path = require('path');
-var mturk = require('mturk-api');
 var _ = require('underscore');
-var program = require('commander');
-var winston = require('winston');
 var NDDB = require('NDDB').NDDB;
 var J = require('JSUS').JSUS;
-
-// Local.
-var version = require('./package.json').version;
 
 // GLOBAL VARIABLES
 ////////////
@@ -23,9 +17,6 @@ var cfg;
 
 // The api command after a connection has been established.
 var api, shapi, options;
-
-// Winston logger.
-var logger;
 
 // Reference to the HITId and HIT object.
 var HITId, HIT;
@@ -50,72 +41,24 @@ var QualificationTypeId, QualificationType;
 var bonusGranted, bonusProcessed;
 
 // Commander.
-
-program
-    .version(version)
-
-     // Specify a configuration file (other inline-options are mixed-in.)
-    .option('-C, --config <confFile>',
-            'Specifies a configuration file')
-
-    .option('-c, --connect',
-            'Opens the connection with Mturk Server')
-
-    .option('-L, --lastHITId',
-            'Fetches the last HIT id by requester')
-
-    .option('-r, --resultsFile <resultsFile>',
-            'Path to a codes file with Exit and Access Codes')
-
-    .option('-i, --inputCodesFile <inputCodesFile>',
-            'Path to a codes file with Exit and Access Codes')
-
-//    .option('-T, --hitTitle [hitTitle]',
-//            'Uses the HITId of the first HIT with same title by requester')
-
-    .option('-Q, --getQualificationTypeId',
-            'Fetches the first qualification type owned by requester from AMT')
-
-    .option('-H, --HITId [HITId]',
-            'HIT id')
-
-    .option('-t, --token [token]',
-            'Unique token for one-time operations')
-
-    .option('-s, --sandbox',
-            'Activate sandbox mode')
-
-    .option('-d, --dry',
-            'Dry-run: does not actually send any request to server')
-
-    .option('-q, --quiet',
-            'No/minimal output printed to console');
-
-
-// Parsing input parameters.
+var program;
+program = require('./lib/program');
 program.parse(process.argv);
 
-// Create logger.
-logger = new winston.Logger({
-    transports: [
-        new (winston.transports.Console)({
-            colorize: true,
-            level: program.quiet ? 'error' : 'silly'
-        }),
-        new (winston.transports.File)({
-            filename: 'log/nodegame-mturk.log',
-            level: 'silly'
-        })
-    ]
-});
-module.exports.logger = logger;
+// Winston logger.
+var logger;
+logger = require('./lib/logger')(program);
+
+return;
 
 // Load and check config.
 /////////////////////////
 
 var shared = require('./lib/shared');
-cfg = shared.loadConfig(program.config);
-cfg = shared.checkConfig(program, cfg);
+
+var cfg = require('./lib/config')(program.config);
+
+
 if (!cfg) return;
 
 // VORPAL COMMANDS
@@ -1250,57 +1193,6 @@ function expireHIT(args, cb) {
 }
 
 
-/**
- * ### connect
- *
- *
- *
- */
-function connect(args, cb) {
-    if (api) {
-        logger.error('already connected.');
-        if (cb) cb();
-        return;
-    }
-
-    logger.info('creating mturk client...');
-
-    // Here we start!
-    mturk.createClient(cfg).then(function(mturkapi) {
-        logger.info('done.');
-
-        ///////////////////////////////////////
-        // Share the api with other commands.
-        api = mturkapi;
-        module.exports.api = api;
-        module.exports.cfg = cfg;
-        // Careful: if there is an error here, vorpal exits without notice.
-        shapi = require('./lib/shared-api.js');
-        ///////////////////////////////////////
-
-        if (args.getQualificationTypeId && args.getLastHITId) {
-            getQualificationType({}, function() {
-                getLastHITId({}, cb);
-            });
-        }
-        else if (args.getLastHITId) {
-            getLastHITId({}, cb);
-        }
-        else if (args.getQualificationTypeId) {
-            getQualificationType({}, cb);
-        }
-        else if (cb) {
-            cb();
-        }
-
-    }).catch(function(err) {
-        logger.err('failed.');
-        logger.error(err);
-        if (cb) cb();
-    });
-
-    return true;
-}
 
 /**
  * ### getQualificationType
