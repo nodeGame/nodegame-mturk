0f187e19d958b620a9a14f36cf801c9d4cc113c2
Stefano Balietti <futur.dorko@gmail.com>
2021-12-17 22:39:23 +0100

    Fixed loading results.

--- ng-amt.js
+++ ng-amt.js
@@ -3,62 +3,59 @@
 "use strict";
 
 // General.
-var fs = require('fs-extra');
-var path = require('path');
-var _ = require('underscore');
-var J = require('JSUS').JSUS;
+const fs = require('fs-extra');
+const path = require('path');
+const _ = require('underscore');
+const J = require('JSUS').JSUS;
 
 // Commander.
 /////////////
 
-var program;
-program = require('./lib/core/program');
+let program = require('./lib/core/program');
 program.parse(process.argv);
 
 // Winston logger.
 //////////////////
 
-var logger;
-logger = require('./lib/core/logger')(program);
+let logger = require('./lib/core/logger')(program);
 
 // Load and check config.
 /////////////////////////
 
-var cfg;
-cfg = require('./lib/core/config')(program);
+let cfg = require('./lib/core/config')(program);
 if (!cfg) return;
 
 // VORPAL COMMANDS
 //////////////////
-var vorpal;
-vorpal = require('./lib/core/vorpal');
+let vorpal = require('./lib/core/vorpal');
 
 
 // DEFAULT ACTION (from program)
 ////////////////////////////////
 
 var codes;
-if (program.inputCodesFile || program.resultsFile || program.game) {
+let opts = program.opts();
+if (opts.inputCodesFile || opts.resultsFile || opts.game) {
     codes = require('./lib/core/codes');
-    if (program.inputCodesFile) {
-        program.path = program.inputCodesFile;
-        codes.loadInputCodes(program);
+    if (opts.inputCodesFile) {
+        opts.path = opts.inputCodesFile;
+        codes.loadInputCodes(opts);
     }
-    if (program.resultsFile) {
-        program.path = program.resultsFile;
-        codes.loadResults(program);
+    if (opts.resultsFile) {
+        opts.path = opts.resultsFile;
+        codes.loadResults(opts);
     }
 }
 var args;
 args = {};
-if (program.getLastHITId) args.getLastHITId = true;
-if (program.getQualificationTypeId) args.getQualificationTypeId = true;
-if (args.getLastHITId || program.getQualificationTypeId) program.connect = true;
+if (opts.getLastHITId) args.getLastHITId = true;
+if (opts.getQualificationTypeId) args.getQualificationTypeId = true;
+if (args.getLastHITId || opts.getQualificationTypeId) opts.connect = true;
 
 // Async operations needs to be completed before the first prompt.
-if (program.connect) {
+if (opts.connect) {
     require('./lib/core/api').connect(args, function() {
-        if (program.game) {
+        if (opts.game) {
             loadGame(startVorpal);
         }
         else {
@@ -66,8 +63,8 @@ if (program.connect) {
         }
     });
 }
-else if (program.game) {
-    loadGame(startVorpal);    
+else if (opts.game) {
+    loadGame(startVorpal);
 }
 else {
     startVorpal();
@@ -83,11 +80,11 @@ function startVorpal() {
 
 function loadGame(cb) {
     // TODO: fix parameter naming.
-    program.path = program.game;
+    opts.path = opts.game;
     // The limit parameter is the next after the game.
-    let indexLimit = program.rawArgs.indexOf(program.game) + 1;
-    program.limit = program.rawArgs[indexLimit];
-    codes.loadGame(program, cb);
+    let indexLimit = opts.rawArgs.indexOf(opts.game) + 1;
+    opts.limit = opts.rawArgs[indexLimit];
+    codes.loadGame(opts, cb);
 }
 
 // Do we need to export it?
